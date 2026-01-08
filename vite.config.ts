51557169619a0047bae812c313883d4383089644
musyafa619 <musyafaarts@gmail.com>
2026-01-08 22:08:23 +0700

    fix: build netlify

--- vite.config.ts
+++ vite.config.ts
@@ -93,12 +93,9 @@ export default defineConfig({
     }),
   ],
   resolve: {
-    alias: [
-      {
-        find: /^src(.+)/,
-        replacement: path.resolve(process.cwd(), 'src/$1'),
-      },
-    ],
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
   },
   server: { port: PORT, host: true },
   preview: { port: PORT, host: true },
